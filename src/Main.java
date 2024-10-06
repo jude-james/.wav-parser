4f93b9b23b6f7eae8efbae259e571c2c9f1ff1b5
Jude James <judejames04@hotmail.co.uk>
2024-10-06 17:25:24 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //String path = "Demo.wav";
+        String path = "/Users/judejames/Desktop/Temp Wav Files/347 Midnight Demons.wav";
+        File file = new File(path);
+
+        WavParser parser = new WavParser(file);
+
+        if (parser.read()) {
+            System.out.println("Success");
+
+            System.out.println(STR."Audio format: \{parser.audioFormat}");
+            System.out.println(STR."Number of channels: \{parser.numChannels}");
+            System.out.println(STR."Sample rate (Hz): \{parser.sampleRate}");
+            System.out.println(STR."Byte rate: \{parser.byteRate}");
+            System.out.println(STR."BlockAlign (Frame size): \{parser.blockAlign}");
+            System.out.println(STR."Bits per sample: \{parser.bitsPerSample}");
+
+            System.out.println(STR."Signed: \{parser.signed}");
+            System.out.println(STR."Big endian: \{parser.endianness}");
+            System.out.println(STR."Duration (seconds): \{parser.duration}");
+
+            // raw sound data
+            byte[] sound = parser.data;
+
+            // samples per channel, e.g. samples[0][1] is the 1st sample of 1st channel
+            float[][] samples = WavParser.getSamples(parser.data, parser.numChannels, parser.blockAlign, parser.bitsPerSample);
+
+            // Creates a copy of the Demo file in my desktop, change path name to whatever you want
+            File newFile = new File("/Users/judejames/desktop/Save Test.wav");
+            WavParser.write(newFile, parser.data, parser.audioFormat, parser.numChannels, parser.sampleRate, parser.byteRate, parser.blockAlign, parser.bitsPerSample);
+        }
+    }
+}
+
+
---
+++ src/WavParser.java
@@ -0,0 +1,218 @@
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
+
+public class WavParser {
+    private final File file;
+
+    // format
+    public int audioFormat;
+    public int numChannels;
+    public float sampleRate;
+    public float byteRate;
+    public int blockAlign;
+    public int bitsPerSample;
+
+    public byte[] data;
+    public boolean signed;
+    public boolean endianness = false; // WAV uses little-endian order (false = little)
+    public float duration;
+
+    public WavParser(File file) {
+        this.file = file;
+    }
+
+    public boolean read() {
+        try {
+            DataInputStream inputStream = new DataInputStream(new FileInputStream(file));
+
+            LinkedHashMap<String, Long> chunkLookup = readSubChunks(inputStream);
+
+            if (chunkLookup == null) {
+                return false;
+            }
+
+            System.out.println(chunkLookup);
+
+            if (chunkLookup.get("fmt ") == null || chunkLookup.get("data") == null) {
+                System.out.println("Perverse wav file. Possible odd length intermediate chunk");
+                return false;
+            }
+
+            long fmtChunkOffset = chunkLookup.get("fmt ");
+            long dataChunkOffset = chunkLookup.get("data");
+
+            inputStream = new DataInputStream(new FileInputStream(file));
+            inputStream.skip(fmtChunkOffset + 8);
+            readFmtChunk(inputStream);
+
+            inputStream = new DataInputStream(new FileInputStream(file));
+            inputStream.skip(dataChunkOffset + 4);
+            readDataChunk(inputStream);
+
+            inputStream.close();
+
+            return true;
+        }
+        catch (IOException e) {
+            System.out.println("Invalid file");
+            return false;
+        }
+    }
+
+    private LinkedHashMap<String, Long> readSubChunks(DataInputStream in) {
+        try {
+            byte[] bytes = new byte[4];
+
+            // Reads the "RIFF" chunk descriptor
+            if (in.read(bytes) < 0) {
+                return null;
+            }
+            String chunkID = new String(bytes, StandardCharsets.ISO_8859_1);
+
+            int chunkSize = Integer.reverseBytes(in.readInt());
+
+            if (in.read(bytes) < 0) {
+                return null;
+            }
+            String format = new String(bytes, StandardCharsets.ISO_8859_1);
+
+            if (!chunkID.equals("RIFF") || !format.equals("WAVE")) {
+                System.out.println("Invalid .wav file");
+                return null;
+            }
+
+            if (file.length() - 8 != chunkSize) {
+                System.out.println(STR."Invalid file size: \{file.length()}. Expected: \{chunkSize + 8}");
+                return null;
+            }
+
+            LinkedHashMap<String, Long> chunkLookup = new LinkedHashMap<>();
+            chunkLookup.put(chunkID, 0L);
+
+            long chunkOffset = 12; // First sub-chunk always starts at byte 12
+
+            // Reads every other chunk descriptor
+            while (in.read(bytes) != -1) {
+                chunkID = new String(bytes, StandardCharsets.ISO_8859_1);
+                chunkLookup.put(chunkID, chunkOffset);
+
+                // read chunk size & skip to next sub-chunk
+                chunkSize = Integer.reverseBytes(in.readInt());
+                long bytesSkipped = in.skip(chunkSize);
+                chunkOffset += bytesSkipped + 8;
+            }
+
+            return chunkLookup;
+        }
+        catch (IOException e) {
+            System.out.println("Invalid file");
+            return null;
+        }
+    }
+
+    private void readFmtChunk(DataInputStream in) {
+        try {
+            audioFormat = Short.reverseBytes(in.readShort());
+            numChannels = Short.reverseBytes(in.readShort());
+            sampleRate = Integer.reverseBytes(in.readInt());
+            byteRate = Integer.reverseBytes(in.readInt());
+            blockAlign = Short.reverseBytes(in.readShort());
+            bitsPerSample = Short.reverseBytes(in.readShort());
+
+            // 8-bit (or lower) sample sizes are always unsigned. 9 bits or higher are always signed
+            signed = bitsPerSample >= 9;
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void readDataChunk(DataInputStream in) {
+        try {
+            int numBytesData = Integer.reverseBytes(in.readInt());
+
+            byte[] data = new byte[numBytesData];
+            in.readFully(data);
+            this.data = data;
+
+            int numSamplesPerChannel = numBytesData / blockAlign;
+            duration = numSamplesPerChannel / sampleRate;
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static float[][] getSamples(byte[] data, int numChannels, int blockAlign, int bitsPerSample) {
+        int numSamplesPerChannel = data.length / blockAlign;
+        float[][] samples = new float[numChannels][numSamplesPerChannel];
+
+        ByteBuffer buffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+
+        if (bitsPerSample != 8 && bitsPerSample != 16 && bitsPerSample != 32) {
+            System.out.println(STR."Unsupported bits per sample: \{bitsPerSample}");
+            return samples;
+        }
+
+        for (int sample = 0; sample < numSamplesPerChannel; sample++) {
+            for (int channel = 0; channel < numChannels; channel++) {
+                if (buffer.position() < data.length) {
+                    if (bitsPerSample == 8) samples[channel][sample] = (buffer.get() & 0xff); // converting to unsigned 0-255
+                    else if (bitsPerSample == 16) samples[channel][sample] = buffer.getShort();
+                    else samples[channel][sample] = buffer.getInt();
+                }
+            }
+        }
+
+        return samples;
+    }
+
+    public static void write(File file, byte[] data, int audioFormat, int numChannels, float sampleRate, float byteRate, int blockAlign, int bitsPerSample) {
+        try {
+            DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(file));
+
+            // Writes RIFF chunk
+            outputStream.writeBytes("RIFF");
+            outputStream.write(intToByteArray(36 + data.length)); // chunkSize (= 36 + subChunk2Size)
+            outputStream.writeBytes("WAVE");
+
+            // Format chunk
+            outputStream.writeBytes("fmt ");
+            outputStream.write(intToByteArray(16)); // subChunk1Size (=16)
+            outputStream.write(shortToByteArray((short) audioFormat));
+            outputStream.write(shortToByteArray((short) numChannels));
+            outputStream.write(intToByteArray((int) sampleRate));
+            outputStream.write(intToByteArray((int) byteRate));
+            outputStream.write(shortToByteArray((short) blockAlign));
+            outputStream.write(shortToByteArray((short) bitsPerSample));
+
+            // Data chunk
+            outputStream.writeBytes("data");
+            outputStream.write(intToByteArray(data.length));
+            outputStream.write(data);
+
+            outputStream.flush();
+            outputStream.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static byte[] intToByteArray(int value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        buffer.putInt(value);
+        return buffer.array();
+    }
+
+    private static byte[] shortToByteArray(short value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Short.BYTES);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        buffer.putShort(value);
+        return buffer.array();
+    }
+}
